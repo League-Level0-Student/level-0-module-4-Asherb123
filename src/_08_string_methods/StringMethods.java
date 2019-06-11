81cc8588fdc69ff68f852df8485a2697dee03dc5
League Student <league@iMac-91.attlocal.net>
2019-06-10 18:25:17 -0700

    I finished two and working on one :sunglasses:

--- src/_08_string_methods/StringMethods.java
+++ src/_08_string_methods/StringMethods.java
@@ -2,22 +2,28 @@ package _08_string_methods;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-
+		String s = JOptionPane.showInputDialog(null, "No you");
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+	char y=	s.charAt(2);
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		
+	System.out.println(y);
+	int a=s.length();
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+		for (int i = 0; i < s.length(); i++) {
+			
+	
+	System.out.println(s.charAt(i));
+		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
@@ -25,6 +31,14 @@ public class StringMethods {
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
 		
+		for (int i = 0; i < s.length(); i++) {
+			if ( s.charAt(i)=='o') {
+				System.out.println(i);
+			}
+		}
+	
+	
+	
 	}
 }
 
