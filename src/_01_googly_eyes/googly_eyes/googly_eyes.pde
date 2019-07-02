a3976dfd237fcc209143e327f14367c8ecd22dea
League Student <league@iMac-17.attlocal.net>
2019-07-01 18:30:00 -0700

    I started prosesing :+1:

---
+++ src/_01_googly_eyes/googly_eyes/googly_eyes.pde
@@ -0,0 +1,11 @@
+void setup(){
+   PImage face = loadImage("eyes.jpg");
+   size(800,600);
+    face.resize(width,height);
+    background(face);
+}
+
+
+void draw(){
+  
+}
--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -10,19 +10,38 @@ public class HappyPet {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 		JOptionPane.showInputDialog("What pet do you want?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
+		for (int i = 0; i < 6; i++) {
+			
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
 			int task = JOptionPane.showOptionDialog(null, "What do you want to do for your new pet?", "Yee", 0, JOptionPane.INFORMATION_MESSAGE, null,
 				new String[] { "Stuff", "Things", "Nothing" }, null);
-
+				System.out.println(task);
 			// 5. Use user input to call the appropriate method created in step 4.
-			
+			if (task==2) {
+				nothing();
+				JOptionPane.showMessageDialog(null, "Your pet got woke.");
+			}
+			if (task==1) {
+				things();
+				JOptionPane.showMessageDialog(null, "Your pet ran away... so you got a new one.");
+			}
+			if (task==0) {
+				stuff();
+				JOptionPane.showMessageDialog(null, "Your pet exploded.");
+			}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+			if (happinessLevel==3) {
+				JOptionPane.showMessageDialog(null, "You love your pet!");
+			
+			break;
+			}
 	}
+	}		
+	
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
@@ -34,7 +53,7 @@ public class HappyPet {
 		happinessLevel--;
 	}
     static void nothing() {
-	happinessLevel++;
+    	happinessLevel++;
 }
 }
 
